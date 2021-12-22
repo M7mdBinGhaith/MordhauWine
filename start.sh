620d439e28f9f02efcd259ebee304e15472beaee
M7mdBinGhaith <96283488+M7mdBinGhaith@users.noreply.github.com>
2021-12-22 10:48:58 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 usermod -u ${UID} steam
 chown steam:steam -R  ${MordhauDIR} ${ConfigDIR}
-gosu steam bash -l -c  '${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir ${MordhauDIR} +login anonymous +app_update ${SteamID} +quit && wine ${MordhauDIR}/MordhauServer.exe -log -Port=${Port} -QueryPort=${QueryPort} -RconPort=${RconPort} -Beaconport=${BeaconPort} -GAMEINI=${ConfigDIR}/Game.ini -ENGINEINI=${ConfigDIR}/Engine.ini'
+gosu steam bash -l -c  '${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir ${MordhauDIR} +login anonymous +app_update ${SteamID} +quit && xvfb-run wine ${MordhauDIR}/MordhauServer.exe -log -Port=${Port} -QueryPort=${QueryPort} -RconPort=${RconPort} -Beaconport=${BeaconPort} -GAMEINI=${ConfigDIR}/Game.ini -ENGINEINI=${ConfigDIR}/Engine.ini'
